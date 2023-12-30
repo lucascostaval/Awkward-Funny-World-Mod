ed6f62238762793196972f9c14fa73c48dc46f00
Lucas Costa Valenca <val.computerscience@gmail.com>
2023-12-29 21:20:15 -0300

    Some modifications

--- src/main/java/net/dotarakotara/awkwardfunnyworldmod/AwkwardFunnyWorldMod.java
+++ src/main/java/net/dotarakotara/awkwardfunnyworldmod/AwkwardFunnyWorldMod.java
@@ -1,5 +1,6 @@
 package net.dotarakotara.awkwardfunnyworldmod;
 
+import net.dotarakotara.awkwardfunnyworldmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -9,11 +10,11 @@ public class AwkwardFunnyWorldMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final String MOD_ID = "awkwardfunnyworld";
+	public static final String MOD_ID = "awkwardfunnyworldmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
